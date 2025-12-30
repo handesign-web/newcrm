60cbb013441778b1c0aa8be7b62c8e1ea87ae0f0
handesign-web <admin@caliteam.xyz>
2025-12-30 22:44:10 +0700

    update

--- config/database.php
+++ config/database.php
@@ -11,6 +11,6 @@ if ($conn->connect_error) {
     die("Koneksi Database Gagal: " . $conn->connect_error);
 }
 
-// Set timezone timestamp sesuai waktu Indonesia
+// Set timezone timestamp sesuai Indonesia
 date_default_timezone_set('Asia/Jakarta');
 ?>
